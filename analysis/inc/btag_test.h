2d35ca427668a4d2bbc6687b158795660aeb7c12
Nathan Hartland <toipot@gmail.com>
2015-01-21 12:52:55 +0000

    added btag headers

--- analysis/inc/btag_test.h
+++ analysis/inc/btag_test.h
@@ -14,11 +14,11 @@
 class bTagTestAnalysis : public Analysis
 {
 	public:
-		bTagTestAnalysis(std::string const& sampleName, std::string analysisName = "bTagTest_hardest4");
+		bTagTestAnalysis(std::string const& sampleName);
 
 		virtual void Analyse(bool const& signal, double const& weight_norm, finalState const&);
 
-	protected:
+	private:
 		// Test bTagging
 		int BTagging(fastjet::PseudoJet const& jet) const; //!< b Tagging method - returns number of consitutent b's
 };
@@ -28,10 +28,14 @@ class bTagTestAnalysis : public Analysis
 	This is a resolved style analysis intended to test the b-Tagging procedure
 	In this case, all jets with pT > 40 GeV are considered
  */
-class bTagTestUCLAnalysis : public bTagTestAnalysis
+class bTagTestUCLAnalysis : public Analysis
 {
 	public:
 		bTagTestUCLAnalysis(std::string const& sampleName);
 
 		virtual void Analyse(bool const& signal, double const& weight_norm, finalState const&);
+
+	private:
+		// Test bTagging
+		int BTagging(fastjet::PseudoJet const& jet) const; //!< b Tagging method - returns number of consitutent b's
 };
