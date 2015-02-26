13a6b2be9a37dcc30c007e5212989762ddde158e
jamesafrost <jamesafrost1@gmail.com>
2015-02-26 20:12:01 +0000

    add instructions

--- analysis/res/spinAllPlots.sh
+++ analysis/res/spinAllPlots.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # takes n arguments, each a directory, the histogram dat files from which are to be passed to plotHist.
+ one argument, the directory "analysis/sample" from which you want to plot the hists
 
-export OUTPUTDIRLOC="plots"
+if [ -z "$OUTPUTDIRLOC" ]; then
+    echo "OUTPUTDIRLOC not set, defaulting to directory plots"
+    export OUTPUTDIRLOC="plots"
+fi
 
 if [ ! -d $OUTPUTDIRLOC/ ]
 then
@@ -12,10 +16,10 @@ fi
 #export FILELIST=`for arg in $@; do echo $arg"/$dat";done`
 #echo "Full list is " $FILELIST
 
-for dat in `ls $1 | grep histo | head -2`
+for dat in `ls $1 | grep histo`
 do {
 export FILELIST=`for arg in $@; do echo $arg"/$dat";done`
-echo "Full list is " $FILELIST
+#echo "Full list is " $FILELIST
 python plotHist.py $FILELIST
 mv histo.pdf $OUTPUTDIRLOC/`echo $dat | sed 's/.dat/.pdf/'`
 #echo $dat
