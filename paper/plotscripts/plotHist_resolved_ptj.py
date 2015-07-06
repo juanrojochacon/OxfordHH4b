08233bf078940df11585f181c6e7ac8afe177d9d
Juan Rojo <juan.rojo@physics.ox.ac.uk>
2015-07-06 15:03:32 +0100

    work on paper

---
+++ paper/plotscripts/plotHist_resolved_ptj.py
@@ -0,0 +1,130 @@
+#! /usr/bin/python
+from matplotlib import pyplot as plt
+from operator import add, sub
+import sys, os, math
+import numpy as np
+
+datafiles = []
+plotnames = []
+
+############################## Settings ###############################
+
+# Output filename
+outfile = "pt_H0_res_C1_boost"
+
+# Datafiles
+datafiles.append("../plotdata/oxford_combined_rw/signal/histo_pt_H0_boost_C1.dat")
+datafiles.append("../plotdata/oxford_combined_rw/background/histo_pt_H0_boost_C1.dat")
+
+# Plot labels
+plotnames.append("Signal")
+plotnames.append("Background")
+
+# Axis labels
+xLabel = "Leading Jet $p_{T}$ (GeV)"
+# yLabel = "$d^2\sigma$/$dp_{T}$ (fb)"
+yLabel = "A. U."
+
+# Log axes
+xLog = False
+yLog = True
+
+# Normalise histograms
+normalise = True
+
+#######################################################################
+
+if len(datafiles)!=len(plotnames):
+  print "Error: datafile and plotname arrays are different lengths!"
+  exit()
+
+colours = ['r', 'b', 'g', 'm', 'c', 'y', 'k']
+icol = 0
+
+# Setup figure
+fig, ax = plt.subplots()
+if xLog == True:
+  ax.set_xscale('log')
+if yLog == True:
+  ax.set_yscale('log')
+
+ax.set_ylabel(yLabel)
+ax.set_xlabel(xLabel)
+
+ax.set_xlim([200,500])
+ax.set_ylim([1e-4,0.05])
+
+for idat in xrange(0,len(datafiles)):
+
+  # Verify paths
+  infilenm = datafiles[idat]
+  if os.path.exists(infilenm) == False:
+    print "Error: source file" + infilenm + " not found!"
+    sys.exit()
+  
+  infile = open(infilenm, 'rb')
+  datafile = open(infilenm, 'rb')
+
+  xhi = []
+  xlo = []
+  yval = []
+
+  errup = []
+  errdn = []
+
+  dataread = False
+  for line in datafile:
+    linesplit = line.split()
+
+    if len(linesplit) == 1:
+      continue
+
+    if linesplit[1] == 'END':
+      break
+
+    if dataread == True:
+      xlo.append(float(linesplit[0]))
+      xhi.append(float(linesplit[1]))
+      yval.append(float(linesplit[2]))
+      errdn.append(float(linesplit[3]))
+      errup.append(float(linesplit[4]))
+
+    if linesplit[1] == 'xlow':
+      dataread = True
+
+  # Normalisations
+  norm = 1.0
+  if normalise == True:
+    norm = 0
+    for i in xrange(0,len(xhi)):
+      h = xhi[i] - xlo[i]
+      norm = norm + h*yval[i]
+  norm = np.sum(norm) # Numpy types
+
+  
+  # Error bars
+  CVup = map(add, yval/norm, errup/norm)
+  CVdn = map(sub, yval/norm, errdn/norm)
+
+  for x in xrange(0,len(xhi)):
+    xvals = [xlo[x], xhi[x]]
+    yvalsup = [CVup[x], CVup[x]]
+    yvalsdn = [CVdn[x], CVdn[x]]
+   # ax.fill_between(xvals, yvalsup, yvalsdn, facecolor=colours[icol], alpha = 0.4, linewidth = 1, color = colours[icol])
+
+  # Insert lower x-values
+  xhi.insert(0,xlo[0])
+  yval.insert(0,yval[0])
+
+  ax.plot(xhi,yval/norm,ls = "steps-pre", color = colours[icol], label=plotnames[idat])
+  icol=icol+1
+
+# Gridlines
+ax.xaxis.grid(True)
+ax.yaxis.grid(True)
+
+# Legend
+legend = ax.legend(loc='best')
+legend.get_frame().set_alpha(0.8)
+
+fig.savefig(outfile+'.pdf')
