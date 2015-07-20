0ae9b8bddd0b32da94981b865804eb326168250f
Nathan Hartland <hartland@nathans-mbp.nat.physics.ox.ac.uk>
2015-07-20 16:48:07 +0100

    Run check

--- analysis/src/run.cc
+++ analysis/src/run.cc
@@ -134,13 +134,13 @@ void InitAnalyses(	std::vector<Analysis*>& HH4bAnalyses,
 	// **************** PLEASE MODIFY  ****************
   //HH4bAnalyses.push_back(new AMCAnalysis("total"));
 
-  HH4bAnalyses.push_back(new OxfordCombinedRWAnalysis("total"));
+  HH4bAnalyses.push_back(new OxfordCombinedCheckAnalysis("total"));
   HH4bAnalyses.push_back(new OxfordCombinedRW2Analysis("total"));
 
-  signalAnalyses.push_back(new OxfordCombinedRWAnalysis("signal"));
+  signalAnalyses.push_back(new OxfordCombinedCheckAnalysis("signal"));
   signalAnalyses.push_back(new OxfordCombinedRW2Analysis("signal"));
 
-  backgroundAnalyses.push_back(new OxfordCombinedRWAnalysis("background"));
+  backgroundAnalyses.push_back(new OxfordCombinedCheckAnalysis("background"));
   backgroundAnalyses.push_back(new OxfordCombinedRW2Analysis("background"));
 
 	// **************** DO NOT MODIFY  ****************
@@ -149,7 +149,7 @@ void InitAnalyses(	std::vector<Analysis*>& HH4bAnalyses,
 
 void InitSampleAnalyses( std::vector<Analysis*>& sampleAnalyses, std::string const& samplename )
 {
-	sampleAnalyses.push_back(new OxfordCombinedRWAnalysis(samplename));
+	sampleAnalyses.push_back(new OxfordCombinedCheckAnalysis(samplename));
 	sampleAnalyses.push_back(new OxfordCombinedRW2Analysis(samplename));
 
 
