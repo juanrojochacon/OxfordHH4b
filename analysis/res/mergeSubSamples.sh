b45cbf260caf1d9d5dd1d53a81e2bc21bd1ae043
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2015-11-09 13:41:50 +0000

    mergeSubSamples

--- analysis/res/mergeSubSamples.sh
+++ analysis/res/mergeSubSamples.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-rm -rf $1/background
-mkdir $1/background
-
-for d in $1/*/
+for d in $1*/
 do
-	echo "Merging " $d
-	for f in $d*.0.yoda; do
+	for f in $d*.2.yoda; do
 		FILENAME=$(basename $f)
 		BASE="${FILENAME%.*}"
-                BASE="${BASE%.*}"
+        BASE="${BASE%.*}"
 
-                echo "Merging " $BASE
+        echo "Merging " $d$BASE
 
-		yodamerge -o $d$BASE.dat $d$BASE.*.yoda
+		yodamerge -o $d$BASE.mrg $d$BASE.*.yoda
 		rm $d$BASE.*.yoda
 	done
+
+	for f in $d*.mrg; do
+		echo "moving "$f
+                FILENAME=$(basename $f)
+                BASE="${FILENAME%.*}"
+		mv $f ./$d$BASE.yoda
+		yoda2flat ./$d$BASE.yoda ./$d$BASE.dat
+	done
+
+	#Merge Ntuples
+	if [[ $d != *background* ]]; then
+    	COUNT=0
+            for f in $d*NTuple.*.dat; do
+            	echo "merging " $f 
+				filename=$(basename $f)
+				basename=${filename:0:9}
+
+				if [[ $COUNT == 0 ]]; then
+					cat $f > $d$basename.dat
+				else
+					cat $f | awk ' NR>1 {print;}' >> $d$basename.dat
+				fi
+
+            	COUNT=$((COUNT+1))
+            done
+    fi
+
+
 done
