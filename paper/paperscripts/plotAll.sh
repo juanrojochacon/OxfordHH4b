94a5606f05622b55dd7c5f9f1757a9f6f7c78195
Juan Rojo <juan.rojo@cern.ch>
2015-11-16 16:48:52 +0000

    added more plotting scripts

--- paper/paperscripts/plotAll.sh
+++ paper/paperscripts/plotAll.sh
@@ -1,10 +1,10 @@
 
 rm -rf *~ *.pdf
 
-for script in MVAdat_noPU Network_bst_noPU
+for script in Hist_mHH_bst_noPU  MVAdat_noPU Network_bst_noPU
 do
 
-    echo "script being executed = " plot$$script.py
+    echo "script being executed = " plot$script.py
 
     python2.7 ./plot$script.py
 
