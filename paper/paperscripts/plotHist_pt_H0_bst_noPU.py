3142d4dab9ac6a2c57feb1ff3076e7b632352b16
Juan Rojo <juan.rojo@cern.ch>
2015-12-10 15:54:27 +0000

    quasi final version of the paper

--- paper/paperscripts/plotHist_pt_H0_bst_noPU.py
+++ paper/paperscripts/plotHist_pt_H0_bst_noPU.py
@@ -124,7 +124,7 @@ ax.xaxis.grid(True)
 ax.yaxis.grid(True)
 
 plt.rcParams.update({'font.size': 16})
-fig.text(0.25,0.93,"Boosted category, $n_{PU}=0$", fontsize=20)
+fig.text(0.25,0.93,r"Boosted category, $\langle n_{PU}\rangle=0$", fontsize=20)
 
 # Legend
 legend = ax.legend(loc='best')
