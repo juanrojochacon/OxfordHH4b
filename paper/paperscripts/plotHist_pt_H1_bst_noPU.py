d27d64ce88911eb9a464ecb8af36ad99f9d5af02
Juan Rojo <juan.rojo@cern.ch>
2015-12-14 08:38:50 +0000

    updated plots

--- paper/paperscripts/plotHist_pt_H1_bst_noPU.py
+++ paper/paperscripts/plotHist_pt_H1_bst_noPU.py
@@ -119,9 +119,6 @@ for idat in xrange(0,len(datafiles)):
   ax.plot(xhi,yval/norm,drawstyle = "steps-pre", color = colours[icol], label=plotnames[idat],linestyle=linestyles[idat],linewidth=2.4)
   icol=icol+1
 
-# Gridlines
-ax.xaxis.grid(True)
-ax.yaxis.grid(True)
 
 plt.rcParams.update({'font.size': 16})
 fig.text(0.25,0.93,r"Boosted category, $\langle n_{PU}\rangle =0$", fontsize=20)
