bca7d1f9e3dab1718a2ce9fa223d3fb3ff9e927e
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-01-12 16:56:50 +0000

    test cut histogram

---
+++ paper-arxiv-v2/paperscripts/nnCutHisto.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python
+import math
+import yoda
+import os
+import string
+
+##################################################################
+
+infile = "~/Dropbox/HH4bMC/mva/results_SK_PU80_trim/bst_SKPU80_trim.dat" # input nTuple
+infile = os.path.expanduser(infile)
+nnfile = "../plotdata/results_SK_PU80/MVA/nn_21X5X3X1_50000-Gen_SKPU80_bst.dat" # input NN discriminant
+ycut = 0.0 # NN discriminant cut
+signal = 1 # switch for signal recognition (1 for signal, 0 for background)
+
+######################### Read nTuple kinematic limits #########################
+
+nTuple = open(infile, 'r')
+kinvar = nTuple.readline().split()[4:] # List of kinematic variables
+nkin = len(kinvar)
+
+kinmin = [ float("inf") for i in kinvar ]
+kinmax = [ 0 for i in kinvar ]
+
+for line in nTuple:
+	var = line.split()[3:]
+	for i in xrange(0,nkin):
+		kinmin[i] = min(kinmin[i], float(var[i]))
+		kinmax[i] = max(kinmax[i], float(var[i]))
+
+################################ Init Histograms ################################
+histos = [ yoda.Histo1D(20, kinmin[i], kinmax[i], title=kinvar[i]) for i in xrange(0,nkin) ]
+	
+# Reopen nTuple
+nTuple = open(infile, 'r')
+nTuple.readline()
+
+# Open NN discriminant file
+NNout = open(nnfile, 'r')
+
+for line in nTuple:
+	var = line.split()[3:]
+	disc = NNout.readline().split()
+
+	assert line.split()[2] == disc[2] # weight check
+	ynn =float(disc[3]) # NN discriminant
+
+	if (ynn > ycut) and disc[1] == str(signal):
+		for i in xrange(0,nkin):
+			histos[i].fill(float(var[i]), float(disc[2]))
+
+ext = '_y'+string.replace(str(ycut),'.','')+'_sig'+str(signal)+'.dat'
+for i in xrange(0,nkin):
+	yoda.core.writeFLAT(histos[i],'histo_'+kinvar[i]+ext)
+
