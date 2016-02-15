ad5c164c8b0ac9b413fca051a9ae22cc8a631d01
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-02-15 11:41:55 +0000

    reintroduced aMC@NLO analysis for checks

---
+++ analysis/inc/amcatnlo.h
@@ -0,0 +1,18 @@
+// ucl.h
+#pragma once
+
+#include "analysis.h"
+#include <iostream>
+#include <vector>
+
+/*
+This is a basic xsec analysis according to the aMC@NLO paper cuts
+ */
+class AMCAnalysis : public Analysis
+{
+	public:
+		AMCAnalysis(std::string const& sampleName, int const& subsample);
+		void Analyse(bool const& signal, double const& weight_norm, finalState const&);
+	private:
+		bool BTagging( fastjet::PseudoJet const& jet ) const;
+};
