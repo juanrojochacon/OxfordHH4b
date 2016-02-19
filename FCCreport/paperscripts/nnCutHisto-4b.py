af7cc3829a12edb4b261452252de158cf8370a21
Juan Rojo <juan.rojo@cern.ch>
2016-02-19 11:41:03 +0000

    Created FCCreport folder

---
+++ FCCreport/paperscripts/nnCutHisto-4b.py
@@ -0,0 +1,64 @@
+#! /usr/local/Cellar/python/2.7.10/bin/python
+import math
+import yoda
+import os
+import string
+
+##################################################################
+
+# input nTuple and NN discriminant
+
+# Boosted category, PU80+SK+Trim
+#infile = "~/Dropbox/HH4bMC/mva/results_SK_PU80_trim/bst_SKPU80_trim.dat"
+# nnfile = "../plotdata/results_SK_PU80/MVA/nn_21X5X3X1_50000-Gen_SKPU80_bst.dat" 
+
+# Resolved category, no PU
+infile = "~/Dropbox/HH4bMC/mva/results_noPU/resNTuple.dat"
+nnfile="../plotdata/results_noPU/MVA/nn_13X5X3X1_50000-Gen_noPU_res.dat"
+
+infile = os.path.expanduser(infile)
+
+# NN discriminant cut
+ycut = 0.0
+# ycut = 0.60 # optimal ANN cut in the resolved category wo PU
+
+######################### Read nTuple kinematic limits #########################
+
+nTuple = open(infile, 'r')
+kinvar = nTuple.readline().split()[4:] # List of kinematic variables
+nkin = len(kinvar)
+
+kinmin = [ float("inf") for i in kinvar ]
+kinmax = [ 0 for i in kinvar ]
+
+for line in nTuple:
+	var = line.split()[3:]
+	for i in xrange(0,nkin):
+		kinmin[i] = min(kinmin[i], float(var[i]))
+		kinmax[i] = max(kinmax[i], float(var[i]))
+
+################################ Init Histograms ################################
+histos = [ yoda.Histo1D(20, kinmin[i], kinmax[i], title=kinvar[i]) for i in xrange(0,nkin) ]
+	
+# Reopen nTuple
+nTuple = open(infile, 'r')
+nTuple.readline()
+
+# Open NN discriminant file
+NNout = open(nnfile, 'r')
+
+for line in nTuple:
+	var = line.split()[3:]
+	disc = NNout.readline().split()
+
+	assert line.split()[2] == disc[2] # weight check
+	ynn =float(disc[3]) # NN discriminant
+
+	if (ynn > ycut)  and disc[0] == "SHERPA_QCD4b" :
+		for i in xrange(0,nkin):
+			histos[i].fill(float(var[i]), float(disc[2]))
+
+ext = '_y'+string.replace(str(ycut),'.','')+'_back4b.dat'
+for i in xrange(0,nkin):
+	yoda.core.writeFLAT(histos[i],'histo_'+kinvar[i]+ext)
+
---
+++ FCCreport/paperscripts/plotHist_pt_H1_bst_noPU.py
@@ -0,0 +1,130 @@
+#! /usr/bin/python
+from matplotlib import pyplot as plt
+from operator import add, sub
+import sys, os, math
+import numpy as np
+
+datafiles = []
+plotnames = []
+
+############################## Settings ###############################
+
+# Output filename
+outfile = "pt_H1_bst_C1d_noPU"
+
+linestyles = [ 'solid', 'dashed']
+
+# Datafiles
+datafiles.append("../plotdata/results_noPU/diHiggs/histo_pt_H1_boost_C1d.dat")
+datafiles.append("../plotdata/results_noPU/background/histo_pt_H1_boost_C1d.dat")
+
+# Plot labels
+plotnames.append("Signal")
+plotnames.append("Background")
+
+# Axis labels
+xLabel = "Sub-leading Higgs Candidate $p^{h}_T$ (GeV)"
+yLabel = "a. u."
+
+# Log axes
+xLog = False
+yLog = True
+
+# Normalise histograms
+normalise = True
+
+#######################################################################
+
+if len(datafiles)!=len(plotnames):
+  print "Error: datafile and plotname arrays are different lengths!"
+  exit()
+
+colours = ['r', 'b', 'g', 'm', 'c', 'y', 'k']
+icol = 0
+
+# Setup figure
+fig, ax = plt.subplots()
+if xLog == True:
+  ax.set_xscale('log')
+if yLog == True:
+  ax.set_yscale('log')
+
+ax.set_ylabel(yLabel)
+ax.set_xlabel(xLabel)
+
+ax.set_xlim([190,600])
+ax.set_ylim([1e-4,0.02])
+
+for idat in xrange(0,len(datafiles)):
+
+  # Verify paths
+  infilenm = datafiles[idat]
+  if os.path.exists(infilenm) == False:
+    print "Error: source file" + infilenm + " not found!"
+    sys.exit()
+  
+  infile = open(infilenm, 'rb')
+  datafile = open(infilenm, 'rb')
+
+  xhi = []
+  xlo = []
+  yval = []
+
+  errup = []
+  errdn = []
+
+  dataread = False
+  for line in datafile:
+    linesplit = line.split()
+
+    if len(linesplit) == 1:
+      continue
+
+    if linesplit[1] == 'END':
+      break
+
+    if dataread == True:
+      xlo.append(float(linesplit[0]))
+      xhi.append(float(linesplit[1]))
+      yval.append(float(linesplit[2]))
+      errdn.append(float(linesplit[3]))
+      errup.append(float(linesplit[4]))
+
+    if linesplit[1] == 'xlow':
+      dataread = True
+
+  # Normalisations
+  norm = 1.0
+  if normalise == True:
+    norm = 0
+    for i in xrange(0,len(xhi)):
+      h = xhi[i] - xlo[i]
+      norm = norm + h*yval[i]
+  norm = np.sum(norm) # Numpy types
+
+  
+  # Error bars
+  CVup = map(add, yval/norm, errup/norm)
+  CVdn = map(sub, yval/norm, errdn/norm)
+
+  for x in xrange(0,len(xhi)):
+    xvals = [xlo[x], xhi[x]]
+    yvalsup = [CVup[x], CVup[x]]
+    yvalsdn = [CVdn[x], CVdn[x]]
+
+  # Insert lower x-values
+  xhi.insert(0,xlo[0])
+  yval.insert(0,yval[0])
+
+  ax.plot(xhi,yval/norm,drawstyle = "steps-pre", color = colours[icol], label=plotnames[idat],linestyle=linestyles[idat],linewidth=2.4)
+  icol=icol+1
+
+
+plt.rcParams.update({'font.size': 16})
+fig.text(0.25,0.93,r"Boosted category, no PU", fontsize=20)
+
+# Legend
+legend = ax.legend(loc='best')
+legend.get_frame().set_alpha(0.8)
+
+fig.savefig(outfile+'.pdf')
