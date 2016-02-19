a417a08195a963ac41fdffc25bac4db3cda24c69
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-02-19 12:12:40 +0000

    cards

--- analysis/res/mergeSubSamples.sh
+++ analysis/res/mergeSubSamples.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
+# This script merges subsample runs of all the samples
+# in a specified directory
 
-rm -rf $1/background
-mkdir $1/background
-
-# First merge the various subsamples
-
-SCOUNT=1
 for d in $1*/
 do
 	if [[ $d != *background* ]]; then
 
 		# Merge subsample yoda files
-		for f in $d*.2.yoda; do
+		for f in $d*.0.yoda; do
 			FILENAME=$(basename $f)
 			BASE="${FILENAME%.*}"
-	        BASE="${BASE%.*}"
+		        BASE="${BASE%.*}"
 
-	        echo "Subsample Merging " $d$BASE
-
+		        echo "Subsample Merging " $d$BASE
 			yodamerge -o $d$BASE.mrg $d$BASE.*.yoda
 			rm $d$BASE.*.yoda
 		done
@@ -49,51 +44,5 @@ do
         
         # Cleanup
         rm $d*NTuple.*.dat
-
-		# Now merge histograms into total background 
-	    if [[ $d != *diHiggs* ]]; then
-			for f in $d*.yoda; do
-				TARGET=$1background/$( basename $f )
-				if [ -f $TARGET ]; 
-				then
-					echo "Total Background Merging " $f
-					yodamerge -o $TARGET $f $TARGET
-				else
-					echo "Total Background Generation " $f
-					cp $f $TARGET
-				fi
-			done
-		fi
     fi
 done
-
-
-# Convert yoda files in background to flat
-for f in $1background/*.yoda
-do
-	filename=$( basename $f )
-	filename="${filename%.*}".dat
-	yoda2flat $f $1background/$filename
-done
-
-
-# Now merge totether nTuples
-for d in $1/*/
-do
-    if [[ $d != *background* ]]; then
-        for f in $d*NTuple.dat; do
-			filename=$(basename $f)
-
-        	if [ -f $1$filename ]; 
-			then
-            	echo "Total Ntuple merging " $f
-            	cat $f | awk ' NR>1 {print;}' >> $1$filename 
-            else
-            	echo "Total NTuple generation " $f
-            	cp $f $1$filename
-        	fi
-        done
-    fi
-done
-
-
