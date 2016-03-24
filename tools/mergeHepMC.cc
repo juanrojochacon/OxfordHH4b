d7c51186f1063cd267564f088ef08f7a0f8b2142
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-03-24 11:54:57 +0000

    updated mergeHepMC

--- tools/mergeHepMC.cc
+++ tools/mergeHepMC.cc
@@ -11,40 +11,20 @@
 // Merge HepMC samples
 // This merging procedure is /VERY SHAKY/ for unweighted events.
 
-std::vector<std::string> getHepMC( std::string pth )
-{
-    std::vector<std::string> files;
-    const char* workingDirectory = pth.c_str();
-    const auto dir = opendir(workingDirectory);
-    if (dir) {
-        auto dirent = readdir(dir);
-        while (dirent) {
-            if (dirent->d_type != DT_DIR) {
-                std::string name{dirent->d_name, dirent->d_namlen};
-                if (name.find(".hepmc") != std::string::npos) 
-                  files.push_back( pth +'/'+ name );
-            }
-            dirent = readdir(dir);
-        }
-        closedir(dir);
-    }
-    return files;
-}
-
-int main( int argc, char* argv[] ) 
+int main( int argc, char** argv ) 
 {  
- if (argc != 2)
+ if (argc < 2)
   {
     std::cerr << "Error: Wrong number of arguments!"<<std::endl;
-    std::cerr << "Usage: mergeHepMC <directory for samples>" <<std::endl;
-    std::cerr << "This code will merge all .hepmc files in the target directory" <<std::endl;
+    std::cerr << "Usage: mergeHepMC <samples>" <<std::endl;
+    std::cerr << "This code will merge all .hepmc files in arguments" <<std::endl;
     exit(-1);
   }
 
   int counter = 0; double sum_weights = 0; double sum_trials = 0;
   HepMC::IO_GenEvent ascii_out("merge.hepmc",std::ios::out);
 
-  const auto files = getHepMC(argv[1]);
+  std::vector<std::string> files(argv + 1, argv + argc);
   for (auto file : files )
   {
     std::cout << "Reading: " <<file <<std::endl;
