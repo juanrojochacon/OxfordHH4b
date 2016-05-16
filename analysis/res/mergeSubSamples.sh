4d1642aa24a1e4041905ae67804d790151ef6763
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2016-05-16 15:28:41 +0100

    fixed yoda merging behavioru

--- analysis/res/mergeSubSamples.sh
+++ analysis/res/mergeSubSamples.sh
@@ -13,7 +13,7 @@ do
 		        BASE="${BASE%.*}"
 
 		        echo "Subsample Merging " $d$BASE
-			yodamerge -o $d$BASE.mrg $d$BASE.*.yoda
+			yodamerge -add -o $d$BASE.mrg $d$BASE.*.yoda
 			rm $d$BASE.*.yoda
 		done
 
