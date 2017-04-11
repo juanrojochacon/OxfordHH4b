b3487a4f8a97882a0a9a580aecefefe245a70ec6
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2017-04-11 17:17:22 +0200

    some annotations

--- mva/lua/data.lua
+++ mva/lua/data.lua
@@ -1,16 +1,33 @@
---- Neural network data sources
+--- HH4b Data sources
+-- This file contains scripts for the management of data nTuples from
+-- particle physics analysis code.
+
 local data = {}
 local thispath = select('1', ...):match(".+%.") or ""
 
 local utils = require('pl.utils')
 require('unsup')
 
+-- A (not particuarly efficient) function to count the number of lines in a file
 local function countLines(targetfile)
 	local iline = 0
 	for line in io.lines(targetfile) do iline = iline+1 end
 	return iline
 end
 
+--- readfile
+-- This function takes a target filename corresponding to a standard NTuple format,
+-- and returns a 'dataset' table.
+
+-- The dataset table has several components:
+-- 	set.dataInputs: a (nData,nKinematics)-sized torch.Tensor holding the input data points
+-- 	set.dataWeight: a (nData)-sized torch.Tensor holding the weight for each input data point
+-- 	set.dataOutput: a (nData)-sized torch ByteTensor holding the 'output' of the datapoints (i.e 1 for signal, 0 for background)
+-- 	set.dataSource: a (nData)-sized list describing the source of each data point.
+-- 	set.nSig:		an integer describing how many signal points are in the sample
+-- 	set.nBkg:		an integer describing now many background points are in the sample
+--  set.signorm:	the overall normalisation for signal events in the training cost
+--  set.bkgnorm:    the overall normalisation for background events in the training cost
 function data.readfile(targetfile, sigfr)
 	local set = {}
 
@@ -35,6 +52,8 @@ function data.readfile(targetfile, sigfr)
 	local sigwgt = 0
 	local bkgwgt = 0
 
+	-- Read datapoints from file, assuming the layout
+	-- <signal flag [0/1]> <source description string> <datapoint weight> <kin1> ... <kinN>
 	for i=1,nData do
 		local line = utils.split(io.read())
 		for j=1, set.nKin do set.dataInputs[i][j] = tonumber(line[j+3]) end
@@ -62,12 +81,16 @@ function data.readfile(targetfile, sigfr)
 	return set
 end
 
+-- Decorrelate the kinematical variables in the dataset by ZCA-whitening
+-- This is a helper function which you can use to try and simplify NN training
 function data.whiten(set)
 	print("Whitening data")
 	set.dataInputs = unsup.zca_whiten(set.dataInputs)
 end
 
--- Walker alias
+-- This function computes the alias table for Walker's alias method.
+-- The alias table is then used for selecting points at random from the
+-- dataset in an efficient manner (see data.wgtpoint)
 function data.aliastable(set)
 	print("Computing alias table")
 	local overfull = {}
@@ -114,6 +137,8 @@ function data.aliastable(set)
 	set.K = K
 end
 
+-- Function that returns a random element index of the dataset
+-- according to the alias method
 function data.wgtpoint(set)
 	local n = #set.U
 	local x = math.random()
@@ -126,6 +151,7 @@ function data.wgtpoint(set)
 	end
 end
 
+-- Generate dataset for torch standard format
 function data.torch(set, size)
 	torchset={};
 	function torchset:size() return #self end
@@ -138,6 +164,7 @@ function data.torch(set, size)
 	return torchset
 end
 
+-- Generate a sub-batch of training elements
 function data.batch(set, size)
 	local inputs = torch.Tensor(size, set.nKin)
 	local labels = torch.DoubleTensor(size)
--- mva/lua/newtorch.lua
+++ mva/lua/newtorch.lua
@@ -6,25 +6,28 @@ local targetfile = arg[1]
 
 print("Reading data ".. targetfile)
 local totalset = data.readfile(targetfile, 0.1)
---print(#totalset._points .. " points read")
 print(collectgarbage("count")/1024, "MB used")
 data.whiten(totalset)
 data.aliastable(totalset)
 
+-- Generate dataset in torch trainer format
 local torchset = data.torch(totalset,1E4)
 
+-- Specify neural network architecture
 mlp = nn.Sequential()
-mlp:add( nn.Linear(totalset.nKin, 10) ) -- 22 input, 5 hidden units
+mlp:add( nn.Linear(totalset.nKin, 10) ) -- 22 input, 10 hidden units
 mlp:add( nn.Sigmoid() )
 mlp:add( nn.Linear(10, 1) ) 
 mlp:add( nn.Sigmoid() ) 
 
+-- Use cross-entropy as cost function for SGD
 criterion = nn.BCECriterion()
 trainer = nn.StochasticGradient(mlp, criterion)
 trainer.learningRate = 0.01
 trainer.maxIteration = 25
 trainer:train(torchset)
 
+-- Write output to file
 local outfile = "fit_new_" .. targetfile
 io.output(outfile)
 for i=1,totalset.nDat,1 do
@@ -33,31 +36,30 @@ end
 
 
 -------------------------------------------------------------------------------------------------------
--- local lumi = 3000
+-- Compute Signal/Background plots
 
--- local outputs = mlp:forward(totalset.dataInputs)
+local lumi = 3000 -- HL-LHC luminosity
 
--- local function passWeight(coords, mask)
--- 	local out = outputs[mask]
--- 	local wgt = totalset.dataWeight[mask]
--- 	return coords:apply( function(v) return lumi*torch.sum(wgt[out:gt(v)]) end ) 
--- end
+local outputs = mlp:forward(totalset.dataInputs)
+local function passWeight(coords, mask)
+	local out = outputs[mask]
+	local wgt = totalset.dataWeight[mask]
+	return coords:apply( function(v) return lumi*torch.sum(wgt[out:gt(v)]) end ) 
+end
 
--- local coords = torch.linspace(0,1,50)
--- local sigThr = passWeight(coords:clone(), totalset.dataOutput)
--- local bkgThr = passWeight(coords:clone(), totalset.dataOutput:ne(1))
+local coords = torch.linspace(0,1,50)
+local sigThr = passWeight(coords:clone(), totalset.dataOutput)
+local bkgThr = passWeight(coords:clone(), totalset.dataOutput:ne(1))
 
--- local SB  = torch.cdiv(sigThr, bkgThr)
--- local SSB = torch.cdiv(sigThr, torch.sqrt(bkgThr))
+local SB  = torch.cdiv(sigThr, bkgThr)
+local SSB = torch.cdiv(sigThr, torch.sqrt(bkgThr))
 
--- gnuplot.epsfigure('sb'..targetfile..'.eps')
--- gnuplot.plot({coords,SB})
+gnuplot.epsfigure('sb'..targetfile..'.eps')
+gnuplot.plot({coords,SB})
+gnuplot.plotflush()
 
--- gnuplot.epsfigure('ssb'..targetfile..'.eps')
--- gnuplot.plot({coords,SSB})
+gnuplot.epsfigure('ssb'..targetfile..'.eps')
+gnuplot.plot({coords,SSB})
+gnuplot.plotflush()
 
--- torch.save("package.dat", totalset)
-
--- -- gnuplot.hist({sigOut,50, 0, 1},{bkgOut,50, 0, 1})
--- gnuplot.plotflush()
-
+torch.save("package.dat", totalset)
