4450fe7f0281c44a1abfaf9b1855753d6e12dc29
Beojan Stanislaus <beojan.stanislaus@physics.ox.ac.uk>
2017-10-17 20:38:02 +0100

    Remove debug statement

--- analysis/src/samples.cc
+++ analysis/src/samples.cc
@@ -197,11 +197,9 @@ void get_final_state_particles(std::ifstream& hepmc_is, finalState& particles,
         cerr << "Info: end of input hepmc file reached" << endl;
         exit(-1);
     }
-    cerr << "Getting final state particles...\n";
 
     HepMC::GenEvent event;
     event.read(hepmc_is);
-    cerr << "Read event.\n";
 
     if (!event.is_valid()) {
         cerr << "Error: Invalid HepMC event!" << endl;
